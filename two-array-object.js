1183e8904ddc658e1be486bd98ef245c78c4cddc
Karly Stark <karlystark@gmail.com>
2023-07-07 16:24:44 -0700

    solved two-array-object

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,18 @@
-// add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+"use strict";
+
+/**  Creates an object that merges two arrays
+ * Accepts two arrays of possibly-different lengths
+ * Returns an object
+ * If not enough values, rest of keys will have value null
+ */
+
+function twoArrayObject(arr1, arr2) {
+let mergedObj = {};
+
+for(let i = 0; i < arr1.length; i++){
+  let key = arr1[i];
+  mergedObj[key] = arr2[i] || null;
 }
+return mergedObj;
+}
+
